97df4c9d7979762a9151430218cf7d632772b0ca
JBenedicto13 <jbenedicto20.jbb@gmail.com>
2022-07-28 21:24:51 +0800

    Initial Commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+
+function Home() {
+    return (
+      <div className="app-home">
+        <div>You are in home</div>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
---
+++ src/components/Todolist.js
@@ -0,0 +1,10 @@
+
+function Todolist() {
+  return (
+    <div className="app-todolist">
+      <div>You are in Todolist</div>
+    </div>
+  );
+}
+
+export default Todolist;
