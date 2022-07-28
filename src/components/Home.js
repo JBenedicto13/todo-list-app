d60fd369cb9d4f44a5307c88c130d3a9963fc07f
JBenedicto13 <jbenedicto20.jbb@gmail.com>
2022-07-29 00:47:08 +0800

    Done, might update when have time for better UI/UX

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+
+    const navigate =  useNavigate()
+
+    const handleOnClick = () => {
+        navigate('/Todolist')
+    }
+
     return (
-      <div className="app-home">
-        <div>You are in home</div>
+      <div className="home-main">
+        <h1>Welcome to my Todo List</h1>
+        <button onClick={handleOnClick}>Start Now!</button>
       </div>
     );
   }
